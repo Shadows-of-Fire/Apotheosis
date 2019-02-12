3584c1eb7fd36fb07cded4a8ce1b3dcd4f83547c
Brennan Ward <Bward7864@gmail.com>
2019-02-12 11:41:36 -0500

    Fix weird boi crash

--- src/main/java/shadows/ench/enchantments/EnchantmentReflective.java
+++ src/main/java/shadows/ench/enchantments/EnchantmentReflective.java
@@ -31,7 +31,7 @@ public class EnchantmentReflective extends Enchantment {
 
 	@Override
 	public boolean canApplyAtEnchantingTable(ItemStack stack) {
-		return stack.getItem().isShield(stack, null) || super.canApplyAtEnchantingTable(stack);
+		return stack.getItem().isShield(stack, null) || stack.getItem().canApplyAtEnchantingTable(stack, this);
 	}
 
 	@Override
