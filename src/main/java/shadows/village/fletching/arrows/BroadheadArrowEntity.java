1d3b04f1b22ed53b2326401c60a1838998e39890
Shadows_of_Fire <Bward7864@gmail.com>
2019-08-24 01:31:02 -0400

    Added scaling damage, may need nerfs

--- src/main/java/shadows/village/fletching/arrows/BroadheadArrowEntity.java
+++ src/main/java/shadows/village/fletching/arrows/BroadheadArrowEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.IPacket;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 import shadows.ApotheosisObjects;
@@ -41,5 +42,15 @@ public class BroadheadArrowEntity extends ArrowEntity {
 	@Override
 	public int getColor() {
 		return -1;
+	}
+
+	@Override
+	protected void arrowHit(LivingEntity living) {
+		EffectInstance bleed = living.getActivePotionEffect(ApotheosisObjects.BLEEDING);
+		if (bleed != null) {
+			bleed.combine(new EffectInstance(ApotheosisObjects.BLEEDING, bleed.getDuration() + 60, bleed.getAmplifier() + 1));
+		} else {
+			living.addPotionEffect(new EffectInstance(ApotheosisObjects.BLEEDING, 300));
+		}
 	}
 }
