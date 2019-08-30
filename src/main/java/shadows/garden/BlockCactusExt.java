7588a755a727ac54d3dc62d5e10be9eda475772c
Brennan Ward <Bward7864@gmail.com>
2019-08-29 22:36:16 -0400

    Fix cactus farms

--- src/main/java/shadows/garden/BlockCactusExt.java
+++ src/main/java/shadows/garden/BlockCactusExt.java
@@ -22,30 +22,32 @@ public class BlockCactusExt extends CactusBlock {
 	@Override
 	public void tick(BlockState state, World world, BlockPos pos, Random random) {
 		if (!world.isAreaLoaded(pos, 1)) return; // Forge: prevent growing cactus from loading unloaded chunks with block update
-		BlockPos blockpos = pos.up();
+		if (!state.isValidPosition(world, pos)) {
+			world.destroyBlock(pos, true);
+		} else {
+			BlockPos blockpos = pos.up();
+			if (pos.getY() != 255 && world.isAirBlock(blockpos)) {
+				int i = 1;
 
-		if (pos.getY() != 255 && world.isAirBlock(blockpos)) {
-			int i = 1;
+				if (GardenModule.maxCactusHeight != 255) for (; world.getBlockState(pos.down(i)).getBlock() == this; ++i)
+					;
 
-			if (GardenModule.maxCactusHeight != 255) for (; world.getBlockState(pos.down(i)).getBlock() == this; ++i)
-				;
+				if (i < GardenModule.maxCactusHeight) {
+					int j = state.get(AGE);
 
-			if (i < GardenModule.maxCactusHeight) {
-				int j = state.get(AGE);
-
-				if (ForgeHooks.onCropsGrowPre(world, blockpos, state, true)) {
-					if (j == 15) {
-						world.setBlockState(blockpos, getDefaultState());
-						BlockState iblockstate = state.with(AGE, Integer.valueOf(0));
-						world.setBlockState(pos, iblockstate, 4);
-						iblockstate.neighborChanged(world, blockpos, this, pos, false);
-					} else {
-						world.setBlockState(pos, state.with(AGE, Integer.valueOf(j + 1)), 4);
+					if (ForgeHooks.onCropsGrowPre(world, blockpos, state, true)) {
+						if (j == 15) {
+							world.setBlockState(blockpos, getDefaultState());
+							BlockState iblockstate = state.with(AGE, Integer.valueOf(0));
+							world.setBlockState(pos, iblockstate, 4);
+							iblockstate.neighborChanged(world, blockpos, this, pos, false);
+						} else {
+							world.setBlockState(pos, state.with(AGE, Integer.valueOf(j + 1)), 4);
+						}
+						ForgeHooks.onCropsGrowPost(world, pos, state);
 					}
-					ForgeHooks.onCropsGrowPost(world, pos, state);
 				}
 			}
 		}
 	}
-
 }
