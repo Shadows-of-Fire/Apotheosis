73c7287b687fb51b752c2d01a614c1730de56b3c
Shadows_of_Fire <Bward7864@gmail.com>
2020-07-02 00:59:31 -0400

    add the affix trade

--- src/main/java/shadows/apotheosis/deadly/loot/affix/AffixEvents.java
+++ src/main/java/shadows/apotheosis/deadly/loot/affix/AffixEvents.java
@@ -55,6 +55,7 @@ import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.BreakSpeed;
 import net.minecraftforge.event.entity.player.PlayerEvent.HarvestCheck;
+import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.Event.Result;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -313,5 +314,10 @@ public class AffixEvents {
 				}
 			}
 		}
+	}
+
+	@SubscribeEvent
+	public void trades(WandererTradesEvent e) {
+		e.getRareTrades().add(new AffixTrade());
 	}
 }
---
+++ src/main/java/shadows/apotheosis/deadly/loot/affix/AffixTrade.java
@@ -0,0 +1,26 @@
+package shadows.apotheosis.deadly.loot.affix;
+
+import java.util.Random;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.merchant.villager.VillagerTrades.ITrade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.MerchantOffer;
+import net.minecraft.util.WeightedRandom;
+import shadows.apotheosis.deadly.loot.LootEntry;
+import shadows.apotheosis.deadly.loot.LootManager;
+import shadows.apotheosis.deadly.loot.LootRarity;
+
+public class AffixTrade implements ITrade {
+
+	@Override
+	public MerchantOffer getOffer(Entity merchant, Random rand) {
+		LootRarity rarity = LootRarity.random(rand);
+		LootEntry entry = WeightedRandom.getRandomItem(rand, LootManager.getEntries());
+		ItemStack loot = LootManager.genLootItem(entry.getStack().copy(), rand, rarity);
+		ItemStack price1 = new ItemStack(Items.EMERALD, 4 + rarity.ordinal() * 12);
+		return new MerchantOffer(price1, ItemStack.EMPTY, loot, 1, 30, 1);
+	}
+
+}
