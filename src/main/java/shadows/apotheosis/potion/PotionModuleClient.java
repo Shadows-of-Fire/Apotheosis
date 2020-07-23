2f171f681361a52c3af5d8bb10fa8435bdb73d51
Shadows_of_Fire <Bward7864@gmail.com>
2020-07-23 11:22:46 -0400

    Attempt to block race condition in item colors

--- src/main/java/shadows/apotheosis/potion/PotionModuleClient.java
+++ src/main/java/shadows/apotheosis/potion/PotionModuleClient.java
@@ -3,15 +3,19 @@ package shadows.apotheosis.potion;
 import net.minecraft.potion.PotionUtils;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import shadows.apotheosis.ApotheosisObjects;
 
+@SuppressWarnings("deprecation")
 public class PotionModuleClient {
 
 	@SubscribeEvent
 	public void colors(ColorHandlerEvent.Item e) {
-		e.getItemColors().register((stack, tint) -> {
-			return PotionUtils.getColor(stack);
-		}, ApotheosisObjects.POTION_CHARM);
+		DeferredWorkQueue.runLater(() -> {
+			e.getItemColors().register((stack, tint) -> {
+				return PotionUtils.getColor(stack);
+			}, ApotheosisObjects.POTION_CHARM);
+		});
 	}
 
 }
