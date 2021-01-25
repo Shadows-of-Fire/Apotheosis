c32879c552dddebaf756849ccdb29ea83a7f91df
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2021-01-24 22:51:12 -0600

    Tweaks re: ChloeDawn

--- src/main/java/shadows/apotheosis/mixin/MixinCactusBlock.java
+++ src/main/java/shadows/apotheosis/mixin/MixinCactusBlock.java
@@ -11,7 +11,7 @@ import shadows.apotheosis.garden.GardenModule;
 @Mixin(CactusBlock.class)
 public class MixinCactusBlock {
 	@ModifyConstant(method = "randomTick", constant = @Constant(intValue = 3))
-	public int modifyMaxCactusHeight(int maxHeightIn) {
+	private int modifyMaxCactusHeight(int maxHeightIn) {
 		int height = GardenModule.maxCactusHeight;
 		return height == 255 ? Integer.MAX_VALUE : height;
 	}
--- src/main/java/shadows/apotheosis/mixin/MixinSugarCaneBlock.java
+++ src/main/java/shadows/apotheosis/mixin/MixinSugarCaneBlock.java
@@ -11,7 +11,7 @@ import shadows.apotheosis.garden.GardenModule;
 @Mixin(SugarCaneBlock.class)
 public class MixinSugarCaneBlock {
 	@ModifyConstant(method = "randomTick", constant = @Constant(intValue = 3))
-	public int modifyMaxSugarCaneHeight(int maxHeightIn) {
+	private int modifyMaxSugarCaneHeight(int maxHeightIn) {
 		int height = GardenModule.maxReedHeight;
 		return height == 255 ? Integer.MAX_VALUE : height;
 	}
