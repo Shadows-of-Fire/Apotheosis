4455451c7cfa0ab5fde9e5190e98b28f31a9a617
Brennan Ward <Bward7864@gmail.com>
2021-10-22 18:11:07 -0400

    Fix the enchantment table not making particles for apoth shelves

--- src/main/java/shadows/apotheosis/ench/table/ApothEnchantBlock.java
+++ src/main/java/shadows/apotheosis/ench/table/ApothEnchantBlock.java
@@ -1,5 +1,7 @@
 package shadows.apotheosis.ench.table;
 
+import java.util.Random;
+
 import javax.annotation.Nullable;
 
 import net.minecraft.block.AbstractBlock;
@@ -10,6 +12,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.INameable;
@@ -18,6 +21,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import shadows.placebo.util.IReplacementBlock;
 
 public class ApothEnchantBlock extends EnchantingTableBlock implements IReplacementBlock {
@@ -70,6 +75,32 @@ public class ApothEnchantBlock extends EnchantingTableBlock implements IReplacem
 	@Override
 	public StateContainer<Block, BlockState> getStateDefinition() {
 		return this.container == null ? super.getStateDefinition() : this.container;
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void animateTick(BlockState state, World level, BlockPos pos, Random rand) {
+		for (int i = -2; i <= 2; ++i) {
+			for (int j = -2; j <= 2; ++j) {
+				if (i > -2 && i < 2 && j == -1) {
+					j = 2;
+				}
+
+				if (rand.nextInt(16) == 0) {
+					for (int k = 0; k <= 1; ++k) {
+						BlockPos blockpos = pos.offset(i, k, j);
+						if (EnchantingStatManager.getEterna(level.getBlockState(blockpos), level, blockpos) > 0) {
+							if (!level.isEmptyBlock(pos.offset(i / 2, 0, j / 2))) {
+								break;
+							}
+
+							level.addParticle(ParticleTypes.ENCHANT, (double) pos.getX() + 0.5D, (double) pos.getY() + 2.0D, (double) pos.getZ() + 0.5D, (double) ((float) i + rand.nextFloat()) - 0.5D, (double) ((float) k - rand.nextFloat() - 1.0F), (double) ((float) j + rand.nextFloat()) - 0.5D);
+						}
+					}
+				}
+			}
+		}
+
 	}
 
 }
