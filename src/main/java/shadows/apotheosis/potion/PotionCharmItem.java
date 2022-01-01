1345376a131ee9d54660acf5a1573aa9d3f21b82
DMehaffy <derrickmehaffy@gmail.com>
2022-01-01 14:59:04 -0700

    Revert 4bdf3779928c6fa763ed05731ef100dfd88596f6
    Reverting the block on repairing potion charms

--- src/main/java/shadows/apotheosis/potion/PotionCharmItem.java
+++ src/main/java/shadows/apotheosis/potion/PotionCharmItem.java
@@ -33,7 +33,7 @@ import shadows.apotheosis.Apotheosis;
 public class PotionCharmItem extends Item {
 
 	public PotionCharmItem() {
-		super(new Item.Properties().stacksTo(1).durability(192).tab(Apotheosis.APOTH_GROUP).setNoRepair());
+		super(new Item.Properties().stacksTo(1).durability(192).tab(Apotheosis.APOTH_GROUP));
 	}
 
 	@Override
