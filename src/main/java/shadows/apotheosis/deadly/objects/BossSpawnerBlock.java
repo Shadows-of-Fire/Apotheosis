296878fde67ed5378c24b46f20e0800562d673bf
Megacrafter127 <ee_player@web.de>
2022-01-04 07:15:22 +0100

    Possible Port BossSpawnerBlock

--- src/main/java/shadows/apotheosis/deadly/objects/BossSpawnerBlock.java
+++ src/main/java/shadows/apotheosis/deadly/objects/BossSpawnerBlock.java
@@ -7,50 +7,43 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.TickableBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import shadows.apotheosis.ApotheosisObjects;
 import shadows.apotheosis.deadly.gen.BossItem;
 import shadows.apotheosis.deadly.reload.BossItemManager;
+import shadows.placebo.block_entity.TickingBlockEntity;
 
-public class BossSpawnerBlock extends Block {
+public class BossSpawnerBlock extends BaseEntityBlock {
 
 	public BossSpawnerBlock(Properties properties) {
 		super(properties);
 	}
 
 	@Override
-	public boolean hasTileEntity(BlockState state) {
-		return true;
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new BossSpawnerTile(pos, state);
 	}
 
-	@Override
-	public BlockEntity createTileEntity(BlockState state, BlockGetter world) {
-		return new BossSpawnerTile();
-	}
-
-	public static class BossSpawnerTile extends BlockEntity implements TickableBlockEntity {
+	public static class BossSpawnerTile extends BlockEntity implements TickingBlockEntity {
 
 		protected BossItem item;
 		protected int ticks = 0;
 
-		public BossSpawnerTile() {
-			super(ApotheosisObjects.BOSS_SPAWN_TILE);
+		public BossSpawnerTile(BlockPos pos, BlockState state) {
+			super(ApotheosisObjects.BOSS_SPAWN_TILE, pos, state);
 		}
 
 		@Override
-		public void tick() {
-			if (this.level.isClientSide) return;
-			if (this.ticks++ % 40 == 0 && this.level.getEntities(EntityType.PLAYER, new AABB(this.worldPosition).inflate(8, 8, 8), EntitySelector.NO_SPECTATORS).stream().anyMatch(p -> !p.isCreative())) {
+		public void serverTick(Level level, BlockPos pos, BlockState state) {
+			if (this.ticks++ % 40 == 0 && level.getEntities(EntityType.PLAYER, new AABB(pos).inflate(8, 8, 8), EntitySelector.NO_SPECTATORS).stream().anyMatch(p -> !p.isCreative())) {
 				this.level.setBlockAndUpdate(this.worldPosition, Blocks.AIR.defaultBlockState());
-				BlockPos pos = this.worldPosition;
 				if (this.item != null) {
-					Mob entity = this.item.createBoss((ServerLevel) this.level, pos, this.level.getRandom());
+					Mob entity = this.item.createBoss((ServerLevel) level, pos, level.getRandom());
 					entity.setPersistenceRequired();
 					this.level.addFreshEntity(entity);
 				}
@@ -68,9 +61,9 @@ public class BossSpawnerBlock extends Block {
 		}
 
 		@Override
-		public void load(BlockState state, CompoundTag tag) {
+		public void load(CompoundTag tag) {
 			this.item = BossItemManager.INSTANCE.getById(new ResourceLocation(tag.getString("boss_item")));
-			super.load(state, tag);
+			super.load(tag);
 		}
 
 	}
