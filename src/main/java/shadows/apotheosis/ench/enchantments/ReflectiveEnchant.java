58cae9248063363945dd5c31df3b1562838e4696
Megacrafter127 <ee_player@web.de>
2022-01-04 04:37:44 +0100

    Port ReflectiveEnchant

--- src/main/java/shadows/apotheosis/ench/enchantments/ReflectiveEnchant.java
+++ src/main/java/shadows/apotheosis/ench/enchantments/ReflectiveEnchant.java
@@ -3,6 +3,7 @@ package shadows.apotheosis.ench.enchantments;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.common.ToolActions;
 import shadows.apotheosis.ench.EnchModule;
 
 public class ReflectiveEnchant extends Enchantment {
@@ -28,7 +29,7 @@ public class ReflectiveEnchant extends Enchantment {
 
 	@Override
 	public boolean canApplyAtEnchantingTable(ItemStack stack) {
-		return super.canApplyAtEnchantingTable(stack) || stack.getItem().isShield(stack, null);
+		return super.canApplyAtEnchantingTable(stack) || stack.canPerformAction(ToolActions.SHIELD_BLOCK);
 	}
 
 }
