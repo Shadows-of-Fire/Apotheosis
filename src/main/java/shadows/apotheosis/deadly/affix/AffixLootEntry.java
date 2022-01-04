53dfd2053cdb2910a2a942c18ef5c01ce6f9e235
Megacrafter127 <ee_player@web.de>
2022-01-04 06:44:06 +0100

    Port AffixLootEntry

--- src/main/java/shadows/apotheosis/deadly/affix/AffixLootEntry.java
+++ src/main/java/shadows/apotheosis/deadly/affix/AffixLootEntry.java
@@ -1,13 +1,13 @@
 package shadows.apotheosis.deadly.affix;
 
-import net.minecraft.util.WeighedRandom;
+import net.minecraft.util.random.WeightedEntry;
 import net.minecraft.world.item.ItemStack;
 
 /**
  * A loot entry represents a possible item that can come out of a loot roll.
  * It is classified into a type, which is used to determine possible affixes.
  */
-public class AffixLootEntry extends WeighedRandom.WeighedRandomItem {
+public class AffixLootEntry extends WeightedEntry.IntrusiveBase {
 
 	protected final ItemStack stack;
 	protected final EquipmentType type;
