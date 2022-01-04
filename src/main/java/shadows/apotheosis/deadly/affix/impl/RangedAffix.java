44441ca7d3660e55c88bf669690231ed83d243c8
Megacrafter127 <ee_player@web.de>
2022-01-04 06:51:50 +0100

    Port RangedAffix

--- src/main/java/shadows/apotheosis/deadly/affix/impl/RangedAffix.java
+++ src/main/java/shadows/apotheosis/deadly/affix/impl/RangedAffix.java
@@ -4,28 +4,29 @@ import java.util.Random;
 
 import javax.annotation.Nullable;
 
+import net.minecraft.util.valueproviders.FloatProvider;
+import net.minecraft.util.valueproviders.UniformFloat;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.storage.loot.RandomValueBounds;
 import shadows.apotheosis.deadly.affix.Affix;
 import shadows.apotheosis.deadly.affix.AffixHelper;
 import shadows.apotheosis.deadly.affix.modifiers.AffixModifier;
 
 public abstract class RangedAffix extends Affix {
 
-	protected final RandomValueBounds range;
+	protected final FloatProvider range;
 
-	public RangedAffix(RandomValueBounds range, int weight) {
+	public RangedAffix(FloatProvider range, int weight) {
 		super(weight);
 		this.range = range;
 	}
 
 	public RangedAffix(float min, float max, int weight) {
-		this(new RandomValueBounds(min, max), weight);
+		this(UniformFloat.of(min, max), weight);
 	}
 
 	@Override
 	public float generateLevel(ItemStack stack, Random rand, @Nullable AffixModifier modifier) {
-		float lvl = this.range.getFloat(rand);
+		float lvl = this.range.sample(rand);
 		if (modifier != null) lvl = modifier.editLevel(this, lvl);
 		AffixHelper.addLore(stack, loreComponent("affix." + this.getRegistryName() + ".desc", String.format("%.2f", lvl)));
 		return lvl;
@@ -33,12 +34,12 @@ public abstract class RangedAffix extends Affix {
 
 	@Override
 	public float getMin() {
-		return this.range.getMin();
+		return this.range.getMinValue();
 	}
 
 	@Override
 	public float getMax() {
-		return this.range.getMax();
+		return this.range.getMaxValue();
 	}
 
 }
