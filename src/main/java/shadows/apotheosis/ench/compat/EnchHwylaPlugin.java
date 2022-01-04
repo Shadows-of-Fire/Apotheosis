db78b2a2c7b41e3de0f172350656228b070b47a9
Megacrafter127 <ee_player@web.de>
2022-01-04 05:52:37 +0100

    Port EnchHwylePlugin

--- src/main/java/shadows/apotheosis/ench/compat/EnchHwylaPlugin.java
+++ src/main/java/shadows/apotheosis/ench/compat/EnchHwylaPlugin.java
@@ -1,18 +1,18 @@
 package shadows.apotheosis.ench.compat;
 
-import java.util.List;
 import java.util.Map;
 
+import mcp.mobius.waila.api.BlockAccessor;
 import mcp.mobius.waila.api.IComponentProvider;
-import mcp.mobius.waila.api.IDataAccessor;
 import mcp.mobius.waila.api.IRegistrar;
 import mcp.mobius.waila.api.IServerDataProvider;
+import mcp.mobius.waila.api.ITooltip;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.TooltipPosition;
 import mcp.mobius.waila.api.WailaPlugin;
 import mcp.mobius.waila.api.config.IPluginConfig;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -29,20 +29,20 @@ public class EnchHwylaPlugin implements IWailaPlugin, IComponentProvider, IServe
 	@Override
 	public void register(IRegistrar reg) {
 		reg.registerComponentProvider(this, TooltipPosition.BODY, ApothAnvilBlock.class);
-		reg.registerBlockDataProvider(this, ApothAnvilBlock.class);
+		reg.registerBlockDataProvider(this, AnvilTile.class);
 	}
 
 	@Override
-	public void appendBody(List<Component> tooltip, IDataAccessor accessor, IPluginConfig config) {
+	public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
 		CompoundTag tag = accessor.getServerData();
-		Map<Enchantment, Integer> enchants = EnchantmentHelper.deserializeEnchantments(tag.getList("enchantments", Constants.NBT.TAG_COMPOUND));
+		Map<Enchantment, Integer> enchants = EnchantmentHelper.deserializeEnchantments(tag.getList("enchantments", Tag.TAG_COMPOUND));
 		for (Map.Entry<Enchantment, Integer> e : enchants.entrySet()) {
 			tooltip.add(e.getKey().getFullname(e.getValue()));
 		}
 	}
 
 	@Override
-	public void appendServerData(CompoundTag tag, ServerPlayer player, Level world, BlockEntity te) {
+	public void appendServerData(CompoundTag tag, ServerPlayer player, Level world, BlockEntity te, boolean showDetails) {
 		if (te instanceof AnvilTile) {
 			ItemStack stack = new ItemStack(Items.ANVIL);
 			EnchantmentHelper.setEnchantments(((AnvilTile) te).getEnchantments(), stack);
