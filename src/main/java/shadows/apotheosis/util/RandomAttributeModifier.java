9c986b37c914bad310578c6619a3e2c52e494e4b
Megacrafter127 <ee_player@web.de>
2022-01-04 04:03:39 +0100

    Port RandomAttributeModifier

--- src/main/java/shadows/apotheosis/util/RandomAttributeModifier.java
+++ src/main/java/shadows/apotheosis/util/RandomAttributeModifier.java
@@ -10,19 +10,19 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.valueproviders.FloatProvider;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
-import net.minecraft.world.level.storage.loot.RandomValueBounds;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RandomAttributeModifier {
 
 	protected final Attribute attribute;
 	protected final Operation op;
-	protected final RandomValueBounds value;
+	protected final FloatProvider value;
 
 	/**
 	 * Creates a Chanced Effect Instance.
@@ -30,7 +30,7 @@ public class RandomAttributeModifier {
 	 * @param effect The effect.
 	 * @param amp A random range of possible amplifiers.
 	 */
-	public RandomAttributeModifier(Attribute attribute, Operation op, RandomValueBounds value) {
+	public RandomAttributeModifier(Attribute attribute, Operation op, FloatProvider value) {
 		this.attribute = attribute;
 		this.op = op;
 		this.value = value;
@@ -38,7 +38,7 @@ public class RandomAttributeModifier {
 
 	public void apply(Random rand, Mob entity) {
 		if (entity == null) throw new RuntimeException("Attempted to apply a random attribute modifier to a null entity!");
-		AttributeModifier modif = new AttributeModifier("apoth_boss_" + this.attribute.getDescriptionId(), this.value.getFloat(rand), this.op);
+		AttributeModifier modif = new AttributeModifier("apoth_boss_" + this.attribute.getDescriptionId(), this.value.sample(rand), this.op);
 		AttributeInstance inst = entity.getAttribute(this.attribute);
 		if (inst == null) throw new RuntimeException(String.format("Attempted to apply a random attribute modifier to an entity (%s) that does not have that attribute (%s)!", entity.getType().getRegistryName(), this.attribute.getRegistryName()));
 		inst.addPermanentModifier(modif);
@@ -51,7 +51,7 @@ public class RandomAttributeModifier {
 			JsonObject obj = json.getAsJsonObject();
 			String _attribute = obj.get("attribute").getAsString();
 			Operation op = ctx.deserialize(obj.get("operation"), Operation.class);
-			RandomValueBounds value = ctx.deserialize(obj.get("value"), RandomValueBounds.class);
+			FloatProvider value = ctx.deserialize(obj.get("value"), FloatProvider.class);
 			Attribute attribute = ForgeRegistries.ATTRIBUTES.getValue(new ResourceLocation(_attribute));
 			if (attribute == null || value == null || op == null) throw new JsonParseException("Attempted to deserialize invalid RandomAttributeModifier: " + json.toString());
 			return new RandomAttributeModifier(attribute, op, value);
