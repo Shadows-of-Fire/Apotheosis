06fb79ad27b4fe951e4d42b87945d593f0ea838f
heipiao233 <heipiao233@outlook.com>
2022-10-26 11:34:20 +0800

    Use 3 Redirects to replace an Overwrite in LivingEntityMixin.java

--- src/main/java/shadows/apotheosis/mixin/LivingEntityMixin.java
+++ src/main/java/shadows/apotheosis/mixin/LivingEntityMixin.java
@@ -1,22 +1,18 @@
 package shadows.apotheosis.mixin;
 
+import net.minecraft.world.effect.MobEffects;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.damagesource.CombatRules;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import shadows.apotheosis.Apoth;
 
 @Mixin(LivingEntity.class)
@@ -26,53 +22,28 @@ public abstract class LivingEntityMixin extends Entity {
 		super(pEntityType, pLevel);
 	}
 
-	/**
-	 * @author Shadows
-	 * @reason Injection of Sundering Potion Effect
-	 * Calculates damage taken based on potions. Required for sundering.
-	 * Called from {@link LivingEntity#getDamageAfterMagicAbsorb(DamageSource, float)}
-	 * TODO: Reduce to @Inject
-	 */
-	@Overwrite
-	public float getDamageAfterMagicAbsorb(DamageSource source, float damage) {
-		if (source.isBypassMagic()) {
-			return damage;
-		} else {
-			float mult = 1;
-			if (this.hasEffect(MobEffects.DAMAGE_RESISTANCE) && source != DamageSource.OUT_OF_WORLD) {
-				int level = this.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() + 1;
-				mult -= 0.2 * level;
-			}
-			if (Apoth.Effects.SUNDERING != null && this.hasEffect(Apoth.Effects.SUNDERING) && source != DamageSource.OUT_OF_WORLD) {
-				int level = this.getEffect(Apoth.Effects.SUNDERING).getAmplifier() + 1;
-				mult += 0.2 * level;
-			}
+	@Redirect(method="getDamageAfterMagicAbsorb", at=@At(value="INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z"))
+	public boolean fakeHasEffect(LivingEntity instance, MobEffect pPotion){
+		return true;
+	}
 
-			float newDamage = damage * mult;
-			float resisted = damage - newDamage;
+	@Redirect(method = "getDamageAfterMagicAbsorb", at=@At(value = "INVOKE", target = "Lnet/minecraft/world/effect/MobEffectInstance;getAmplifier()I"))
+	public int fakeGetAmplifier(MobEffectInstance instance) {
+		return -1;
+	}
 
-			if (resisted > 0.0F && resisted < 3.4028235E37F) {
-				if ((Object) this instanceof ServerPlayer sp) {
-					sp.awardStat(Stats.CUSTOM.get(Stats.DAMAGE_RESISTED), Math.round(resisted * 10.0F));
-				} else if (source.getEntity() instanceof ServerPlayer sp) {
-					sp.awardStat(Stats.CUSTOM.get(Stats.DAMAGE_DEALT_RESISTED), Math.round(resisted * 10.0F));
-				}
-			}
-
-			damage = newDamage;
-
-			if (damage <= 0.0F) {
-				return 0.0F;
-			} else {
-				int k = EnchantmentHelper.getDamageProtection(this.getArmorSlots(), source);
-
-				if (k > 0) {
-					damage = CombatRules.getDamageAfterMagicAbsorb(damage, k);
-				}
-
-				return damage;
-			}
+	@Redirect(method = "getDamageAfterMagicAbsorb", at=@At(value = "INVOKE", target = "Ljava/lang/Math;max(FF)F"))
+	public float fakeMax(float a, float b) {
+		float mult = 1;
+		if (this.hasEffect(MobEffects.DAMAGE_RESISTANCE)) {
+			int level = this.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() + 1;
+			mult -= 0.2 * level;
 		}
+		if (Apoth.Effects.SUNDERING != null && this.hasEffect(Apoth.Effects.SUNDERING)) {
+			int level = this.getEffect(Apoth.Effects.SUNDERING).getAmplifier() + 1;
+			mult += 0.2 * level;
+		}
+		return a * mult;
 	}
 
 	@Shadow
