436ef08e746ac05291406e61169756f1b08bd9de
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2022-12-08 00:11:36 -0500

    Replace sundering overwrite with redirects

--- src/main/java/shadows/apotheosis/mixin/LivingEntityMixin.java
+++ src/main/java/shadows/apotheosis/mixin/LivingEntityMixin.java
@@ -1,21 +1,19 @@
 package shadows.apotheosis.mixin;
 
+import javax.annotation.Nullable;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.damagesource.CombatRules;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import shadows.apotheosis.Apoth;
 
@@ -28,51 +26,37 @@ public abstract class LivingEntityMixin extends Entity {
 
 	/**
 	 * @author Shadows
-	 * @reason Injection of Sundering Potion Effect
-	 * Calculates damage taken based on potions. Required for sundering.
-	 * Called from {@link LivingEntity#getDamageAfterMagicAbsorb(DamageSource, float)}
-	 * TODO: Reduce to @Inject
+	 * @reason Injection of the Sundering potion effect, which is applied during resistance calculations.
+	 * @param value Damage modifier percentage after resistance has been applied [1.0, -inf]
+	 * @param max Zero
+	 * @param source The damage source
+	 * @param damage The initial damage amount
 	 */
-	@Overwrite
-	public float getDamageAfterMagicAbsorb(DamageSource source, float damage) {
-		if (source.isBypassMagic()) {
-			return damage;
-		} else {
-			float mult = 1;
-			if (this.hasEffect(MobEffects.DAMAGE_RESISTANCE) && source != DamageSource.OUT_OF_WORLD) {
-				int level = this.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() + 1;
-				mult -= 0.2 * level;
-			}
-			if (Apoth.Effects.SUNDERING.isPresent() && this.hasEffect(Apoth.Effects.SUNDERING.get()) && source != DamageSource.OUT_OF_WORLD) {
-				int level = this.getEffect(Apoth.Effects.SUNDERING.get()).getAmplifier() + 1;
-				mult += 0.2 * level;
-			}
-
-			float newDamage = damage * mult;
-			float resisted = damage - newDamage;
-
-			if (resisted > 0.0F && resisted < 3.4028235E37F) {
-				if ((Object) this instanceof ServerPlayer sp) {
-					sp.awardStat(Stats.CUSTOM.get(Stats.DAMAGE_RESISTED), Math.round(resisted * 10.0F));
-				} else if (source.getEntity() instanceof ServerPlayer sp) {
-					sp.awardStat(Stats.CUSTOM.get(Stats.DAMAGE_DEALT_RESISTED), Math.round(resisted * 10.0F));
-				}
-			}
-
-			damage = newDamage;
-
-			if (damage <= 0.0F) {
-				return 0.0F;
-			} else {
-				int k = EnchantmentHelper.getDamageProtection(this.getArmorSlots(), source);
-
-				if (k > 0) {
-					damage = CombatRules.getDamageAfterMagicAbsorb(damage, k);
-				}
-
-				return damage;
-			}
+	@Redirect(at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(FF)F"), method = "getDamageAfterMagicAbsorb(Lnet/minecraft/world/damagesource/DamageSource;F)F")
+	public float apoth_sundering_max(float value, float max, DamageSource source, float damage) {
+		if (Apoth.Effects.SUNDERING.isPresent() && this.hasEffect(Apoth.Effects.SUNDERING.get()) && source != DamageSource.OUT_OF_WORLD) {
+			int level = this.getEffect(Apoth.Effects.SUNDERING.get()).getAmplifier() + 1;
+			value += damage * level * 0.2F;
 		}
+		return Math.max(value, max);
+	}
+
+	/**
+	 * @author Shadows
+	 * @reason Used to enter an if-condition so the above mixin always triggers.
+	 */
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z"), method = "getDamageAfterMagicAbsorb(Lnet/minecraft/world/damagesource/DamageSource;F)F")
+	public boolean apoth_sundering_hasEffect(LivingEntity ths, MobEffect effect) {
+		return true;
+	}
+
+	/**
+	 * @author Shadows
+	 * @reason Used to prevent an NPE since we're faking true on hasEffect
+	 */
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/effect/MobEffectInstance;getAmplifier()I"), method = "getDamageAfterMagicAbsorb(Lnet/minecraft/world/damagesource/DamageSource;F)F")
+	public int apoth_sundering_getAmplifier(@Nullable MobEffectInstance inst) {
+		return inst == null ? -1 : inst.getAmplifier();
 	}
 
 	@Shadow
