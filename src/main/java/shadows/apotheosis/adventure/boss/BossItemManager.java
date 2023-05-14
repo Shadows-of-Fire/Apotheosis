8a49fed64ae71acc0f907eb4920dddffd83a07bb
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-05-14 03:35:31 -0700

    #513

--- src/main/java/shadows/apotheosis/adventure/boss/BossItemManager.java
+++ src/main/java/shadows/apotheosis/adventure/boss/BossItemManager.java
@@ -1,5 +1,12 @@
 package shadows.apotheosis.adventure.boss;
 
+import java.util.Map;
+
+import com.google.gson.JsonElement;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
 import shadows.apotheosis.adventure.AdventureModule;
 import shadows.placebo.json.WeightedJsonReloadListener;
 
@@ -9,6 +16,14 @@ public class BossItemManager extends WeightedJsonReloadListener<BossItem> {
 
 	public BossItemManager() {
 		super(AdventureModule.LOGGER, "bosses", false, false);
+	}
+
+	@Override
+	protected Map<ResourceLocation, JsonElement> prepare(ResourceManager pResourceManager, ProfilerFiller pProfiler) {
+		var map = super.prepare(pResourceManager, pProfiler);
+		// The author of Brutal Bosses continues to use my subkey, so, here we go doing stupid shit to work around it.
+		map.keySet().removeIf(r -> r.getNamespace().equals("brutalbosses"));
+		return map;
 	}
 
 	@Override
