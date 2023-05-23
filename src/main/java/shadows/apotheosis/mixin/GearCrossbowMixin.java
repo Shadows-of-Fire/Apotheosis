bc560fdd3754bf4efc0860c0a09622bb42c1b35d
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-05-22 21:56:14 -0700

    format

--- src/main/java/shadows/apotheosis/mixin/GearCrossbowMixin.java
+++ src/main/java/shadows/apotheosis/mixin/GearCrossbowMixin.java
@@ -1,4 +1,5 @@
 package shadows.apotheosis.mixin;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,30 +15,28 @@ import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import shadows.apotheosis.ench.enchantments.masterwork.CrescendoEnchant;
 
 @Pseudo
 @Mixin(targets = "net.silentchaos512.gear.item.gear.GearCrossbowItem")
 public class GearCrossbowMixin extends CrossbowItem {
 
-    public GearCrossbowMixin(Properties pProperties) {
-        super(pProperties);
-    }
+	public GearCrossbowMixin(Properties pProperties) {
+		super(pProperties);
+	}
 
-    @Inject(method = "fireProjectiles", at = @At(value = "HEAD", remap = false))
-    private static void apoth_preFired(Level pLevel, LivingEntity pPlayer, InteractionHand pHand, ItemStack crossbow, float velocity, float inaccuracy, CallbackInfo ci) {
-        CrescendoEnchant.preArrowFired(pPlayer.getItemInHand(pHand));
-    }
+	@Inject(method = "fireProjectiles", at = @At(value = "HEAD", remap = false))
+	private static void apoth_preFired(Level pLevel, LivingEntity pPlayer, InteractionHand pHand, ItemStack crossbow, float velocity, float inaccuracy, CallbackInfo ci) {
+		CrescendoEnchant.preArrowFired(pPlayer.getItemInHand(pHand));
+	}
 
-    @Inject(method = "m_7203_", at = @At(value = "RETURN", ordinal = 1))
-    public void apoth_addCharges(Level pLevel, Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> ci) {
-        CrescendoEnchant.onArrowFired(pPlayer.getItemInHand(pHand));
-    }
+	@Inject(method = "m_7203_", at = @At(value = "RETURN", ordinal = 1))
+	public void apoth_addCharges(Level pLevel, Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> ci) {
+		CrescendoEnchant.onArrowFired(pPlayer.getItemInHand(pHand));
+	}
 
-    @Inject(method = "getArrow", at = @At(value = "RETURN"), remap = false)
-    private static void apoth_markArrows(Level pLevel, LivingEntity pLivingEntity, ItemStack pCrossbowStack, ItemStack pAmmoStack, CallbackInfoReturnable<AbstractArrow> ci) {
-        CrescendoEnchant.markGeneratedArrows(ci.getReturnValue(), pCrossbowStack);
-    }
+	@Inject(method = "getArrow", at = @At(value = "RETURN"), remap = false)
+	private static void apoth_markArrows(Level pLevel, LivingEntity pLivingEntity, ItemStack pCrossbowStack, ItemStack pAmmoStack, CallbackInfoReturnable<AbstractArrow> ci) {
+		CrescendoEnchant.markGeneratedArrows(ci.getReturnValue(), pCrossbowStack);
+	}
 }
-
