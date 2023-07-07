ec0021e824eae90fe0dbb41a59e28a8be4c2e895
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-07-07 00:26:58 -0700

    minor ench book tooltips

--- src/main/java/shadows/apotheosis/ench/EnchModuleClient.java
+++ src/main/java/shadows/apotheosis/ench/EnchModuleClient.java
@@ -1,5 +1,7 @@
 package shadows.apotheosis.ench;
 
+import java.util.List;
+
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
@@ -8,6 +10,7 @@ import net.minecraft.client.renderer.blockentity.EnchantTableRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -39,8 +42,9 @@ public class EnchModuleClient {
 	@SubscribeEvent
 	public void tooltips(ItemTooltipEvent e) {
 		Item i = e.getItemStack().getItem();
-		if (i == Items.COBWEB) e.getToolTip().add(Component.translatable("info.apotheosis.cobweb").withStyle(ChatFormatting.GRAY));
-		else if (i == Apoth.Items.PRISMATIC_WEB.get()) e.getToolTip().add(Component.translatable("info.apotheosis.prismatic_cobweb").withStyle(ChatFormatting.GRAY));
+		List<Component> tooltip = e.getToolTip();
+		if (i == Items.COBWEB) tooltip.add(Component.translatable("info.apotheosis.cobweb").withStyle(ChatFormatting.GRAY));
+		else if (i == Apoth.Items.PRISMATIC_WEB.get()) tooltip.add(Component.translatable("info.apotheosis.prismatic_cobweb").withStyle(ChatFormatting.GRAY));
 		else if (i instanceof BlockItem) {
 			Block block = ((BlockItem) i).getBlock();
 			Level world = Minecraft.getInstance().level;
@@ -65,39 +69,56 @@ public class EnchModuleClient {
 			float rectification = EnchantingStatManager.getQuantaRectification(state, world, BlockPos.ZERO);
 			int clues = EnchantingStatManager.getBonusClues(state, world, BlockPos.ZERO);
 			if (eterna != 0 || quanta != 0 || arcana != 0 || rectification != 0 || clues != 0) {
-				e.getToolTip().add(Component.translatable("info.apotheosis.ench_stats").withStyle(ChatFormatting.GOLD));
+				tooltip.add(Component.translatable("info.apotheosis.ench_stats").withStyle(ChatFormatting.GOLD));
 			}
 			if (eterna != 0) {
 				if (eterna > 0) {
-					e.getToolTip().add(Component.translatable("info.apotheosis.eterna.p", String.format("%.2f", eterna), String.format("%.2f", maxEterna)).withStyle(ChatFormatting.GREEN));
-				} else e.getToolTip().add(Component.translatable("info.apotheosis.eterna", String.format("%.2f", eterna)).withStyle(ChatFormatting.GREEN));
+					tooltip.add(Component.translatable("info.apotheosis.eterna.p", String.format("%.2f", eterna), String.format("%.2f", maxEterna)).withStyle(ChatFormatting.GREEN));
+				} else tooltip.add(Component.translatable("info.apotheosis.eterna", String.format("%.2f", eterna)).withStyle(ChatFormatting.GREEN));
 			}
 			if (quanta != 0) {
-				e.getToolTip().add(Component.translatable("info.apotheosis.quanta" + (quanta > 0 ? ".p" : ""), String.format("%.2f", quanta)).withStyle(ChatFormatting.RED));
+				tooltip.add(Component.translatable("info.apotheosis.quanta" + (quanta > 0 ? ".p" : ""), String.format("%.2f", quanta)).withStyle(ChatFormatting.RED));
 			}
 			if (arcana != 0) {
-				e.getToolTip().add(Component.translatable("info.apotheosis.arcana" + (arcana > 0 ? ".p" : ""), String.format("%.2f", arcana)).withStyle(ChatFormatting.DARK_PURPLE));
+				tooltip.add(Component.translatable("info.apotheosis.arcana" + (arcana > 0 ? ".p" : ""), String.format("%.2f", arcana)).withStyle(ChatFormatting.DARK_PURPLE));
 			}
 			if (rectification != 0) {
-				e.getToolTip().add(Component.translatable("info.apotheosis.rectification" + (rectification > 0 ? ".p" : ""), String.format("%.2f", rectification)).withStyle(ChatFormatting.YELLOW));
+				tooltip.add(Component.translatable("info.apotheosis.rectification" + (rectification > 0 ? ".p" : ""), String.format("%.2f", rectification)).withStyle(ChatFormatting.YELLOW));
 			}
 			if (clues != 0) {
-				e.getToolTip().add(Component.translatable("info.apotheosis.clues" + (clues > 0 ? ".p" : ""), String.format("%d", clues)).withStyle(ChatFormatting.DARK_AQUA));
+				tooltip.add(Component.translatable("info.apotheosis.clues" + (clues > 0 ? ".p" : ""), String.format("%d", clues)).withStyle(ChatFormatting.DARK_AQUA));
 			}
 		} else if (i == Items.ENCHANTED_BOOK) {
 			ItemStack stack = e.getItemStack();
 			var enchMap = EnchantmentHelper.getEnchantments(stack);
 			if (enchMap.size() == 1) {
 				var ench = enchMap.keySet().iterator().next();
+				int lvl = enchMap.values().iterator().next();
 				if (!ModList.get().isLoaded("enchdesc")) {
 					if (ForgeRegistries.ENCHANTMENTS.getKey(ench).getNamespace().equals(Apotheosis.MODID)) {
-						e.getToolTip().add(Component.translatable(ench.getDescriptionId() + ".desc").withStyle(ChatFormatting.DARK_GRAY));
+						tooltip.add(Component.translatable(ench.getDescriptionId() + ".desc").withStyle(ChatFormatting.DARK_GRAY));
 					}
 				}
-				var enchInfo = EnchModule.getEnchInfo(ench);
-				
+				var info = EnchModule.getEnchInfo(ench);
+				if (e.getFlags().isAdvanced()) {
+					tooltip.add(Component.translatable("Power Range: [%s, %s]", info.getMinPower(lvl), info.getMaxPower(lvl)).withStyle(ChatFormatting.GREEN));
+				}
+				Object[] args = new Object[4];
+				args[0] = boolComp("info.apotheosis.discoverable", info.isDiscoverable());
+				args[1] = boolComp("info.apotheosis.lootable", info.isLootable());
+				args[2] = boolComp("info.apotheosis.tradeable", info.isTradeable());
+				args[3] = boolComp("info.apotheosis.treasure", info.isTreasure());
+				if (e.getFlags().isAdvanced()) {
+					tooltip.add(Component.translatable("%s \u2507 %s \u2507 %s \u2507 %s", args[0], args[1], args[2], args[3]).withStyle(ChatFormatting.DARK_GRAY));
+				} else {
+					tooltip.add(Component.translatable("%s \u2507 %s", args[2], args[3]).withStyle(ChatFormatting.DARK_GRAY));
+				}
 			}
 		}
+	}
+
+	private static Component boolComp(String key, boolean flag) {
+		return Component.translatable(key + (flag ? "" : ".not")).withStyle(Style.EMPTY.withColor(flag ? 0x108810 : 0xAA1616));
 	}
 
 	public static void init() {
