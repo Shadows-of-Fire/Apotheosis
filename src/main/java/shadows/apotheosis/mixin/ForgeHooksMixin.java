ac392a2ec19c89fd00953e60185da188370a5fde
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-07-21 15:26:43 -0700

    add missing remap=false

--- src/main/java/shadows/apotheosis/mixin/ForgeHooksMixin.java
+++ src/main/java/shadows/apotheosis/mixin/ForgeHooksMixin.java
@@ -19,7 +19,7 @@ import shadows.placebo.json.PlaceboJsonReloadListener;
 @Mixin(ForgeHooks.class)
 public class ForgeHooksMixin {
 
-	@Inject(at = @At("HEAD"), method = "loadLootTable(Lcom/google/gson/Gson;Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonElement;ZLnet/minecraft/world/level/storage/loot/LootTables;)Lnet/minecraft/world/level/storage/loot/LootTable;", cancellable = true, require = 1)
+	@Inject(at = @At("HEAD"), method = "loadLootTable(Lcom/google/gson/Gson;Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonElement;ZLnet/minecraft/world/level/storage/loot/LootTables;)Lnet/minecraft/world/level/storage/loot/LootTable;", cancellable = true, require = 1, remap = false)
 	private static void apoth_lootTableConditions(Gson gson, ResourceLocation name, JsonElement data, boolean custom, LootTables mgr, CallbackInfoReturnable<LootTable> cir) {
 		if (!PlaceboJsonReloadListener.checkConditions(data, name, "loot_table", Placebo.LOGGER, IContext.EMPTY)) {
 			cir.setReturnValue(LootTable.EMPTY);
