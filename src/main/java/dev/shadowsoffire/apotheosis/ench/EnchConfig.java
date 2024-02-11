e61b84b5de380583b9ef03fc3f84bea0dcae4fb7
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-02-10 23:07:33 -0800

    config note

--- src/main/java/dev/shadowsoffire/apotheosis/ench/EnchConfig.java
+++ src/main/java/dev/shadowsoffire/apotheosis/ench/EnchConfig.java
@@ -11,7 +11,7 @@ public class EnchConfig {
         c.setTitle("Apotheosis Enchantment Module Config");
 
         showEnchantedBookMetadata = c.getBoolean("Show Enchanted Book Metadata", "client", true, "If enchanted book metadata (treasure, tradeable, etc) are shown in the tooltip.");
-        sculkShelfNoiseChance = c.getInt("Sculkshelf Noise Chance", "client", 200, 0, 32767, "The 1/n chance that a sculkshelf plays a sound, per client tick.");
+        sculkShelfNoiseChance = c.getInt("Sculkshelf Noise Chance", "client", 200, 0, 32767, "The 1/n chance that a sculkshelf plays a sound, per client tick. Set to 0 to disable.");
 
         if (c.hasChanged()) c.save();
     }
