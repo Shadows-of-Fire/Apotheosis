5ec51a86e1214b1707848696fa35b9c0e80b124f
Kai <kai.zahn@yahoo.de>
2024-03-13 19:58:17 +0100

    fix block transform not properly working

--- src/main/java/shadows/apotheosis/adventure/affix/socket/gem/bonus/special/DropTransformBonus.java
+++ src/main/java/shadows/apotheosis/adventure/affix/socket/gem/bonus/special/DropTransformBonus.java
@@ -1,7 +1,5 @@
 package shadows.apotheosis.adventure.affix.socket.gem.bonus.special;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import com.google.common.base.Preconditions;
@@ -10,7 +8,6 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.tags.TagKey;
@@ -19,11 +16,9 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.minecraftforge.fml.util.thread.EffectiveSide;
 import shadows.apotheosis.Apotheosis;
 import shadows.apotheosis.adventure.affix.Affix;
 import shadows.apotheosis.adventure.affix.socket.gem.GemClass;
-import shadows.apotheosis.adventure.affix.socket.gem.GemManager;
 import shadows.apotheosis.adventure.affix.socket.gem.bonus.GemBonus;
 import shadows.apotheosis.adventure.loot.LootRarity;
 import shadows.placebo.codec.PlaceboCodecs.IngredientCodec;
@@ -62,8 +57,6 @@ public class DropTransformBonus extends GemBonus {
     protected final Map<LootRarity, StepFunction> values;
     protected final String descKey;
 
-    protected final transient List<Block> blocks;
-
     public DropTransformBonus(GemClass gemClass, TagKey<Block> tag, Ingredient inputs, ItemStack output, Map<LootRarity, StepFunction> values, String descKey) {
         super(Apotheosis.loc("drop_transform"), gemClass);
         this.tag = tag;
@@ -71,10 +64,6 @@ public class DropTransformBonus extends GemBonus {
         this.output = output;
         this.values = values;
         this.descKey = descKey;
-        if (EffectiveSide.get().isServer()) {
-            this.blocks = GemManager.INSTANCE._getContext().getTag(tag).stream().map(Holder::get).toList();
-        }
-        else this.blocks = Collections.emptyList();
     }
 
     @Override
@@ -91,8 +80,7 @@ public class DropTransformBonus extends GemBonus {
     @Override
     public void modifyLoot(ItemStack gem, LootRarity rarity, ObjectArrayList<ItemStack> loot, LootContext ctx) {
         if (ctx.hasParam(LootContextParams.BLOCK_STATE)) {
-            Block block = ctx.getParam(LootContextParams.BLOCK_STATE).getBlock();
-            if (!this.blocks.isEmpty() && !this.blocks.contains(block)) return;
+            if (!ctx.getParam(LootContextParams.BLOCK_STATE).is(tag)) return;
             if (ctx.getRandom().nextFloat() <= this.values.get(rarity).min()) {
                 for (int i = 0; i < loot.size(); i++) {
                     ItemStack stack = loot.get(i);
