ba02ee67bdc8896b0a85060051c9b49dfc362985
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-04-10 18:48:48 -0700

    Migrate override mixin to inject

---
+++ src/main/java/dev/shadowsoffire/apotheosis/mixin/EntityMixin.java
@@ -0,0 +1,33 @@
+package dev.shadowsoffire.apotheosis.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+
+@Mixin(value = Entity.class, remap = false)
+public abstract class EntityMixin {
+
+    @Shadow
+    abstract Component getCustomName();
+
+    /**
+     * Used to make the glowing effect on mobs use their name color.
+     */
+    @Inject(method = "getTeamColor", at = @At("RETURN"), cancellable = true)
+    public void apoth_getTeamColor(CallbackInfoReturnable<Integer> cir) {
+        int color = cir.getReturnValueI();
+        if (color == 16777215) {
+            Component name = this.getCustomName();
+            if (name != null && name.getStyle().getColor() != null) {
+                color = name.getStyle().getColor().getValue();
+            }
+        }
+        cir.setReturnValue(color);
+    }
+
+}
