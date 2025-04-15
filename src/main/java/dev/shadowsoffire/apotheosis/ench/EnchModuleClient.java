dceb761967fd6d10325e2d7e0dd3cfafdd74bb3b
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-04-14 22:03:38 -0700

    Fix hang in anvil screen
    Closes #1482

--- src/main/java/dev/shadowsoffire/apotheosis/ench/EnchModuleClient.java
+++ src/main/java/dev/shadowsoffire/apotheosis/ench/EnchModuleClient.java
@@ -152,7 +152,7 @@ public class EnchModuleClient {
     public void drawAnvilCostBlob(ScreenEvent.Render.Post e) {
         if (e.getScreen() instanceof AnvilScreen anv) {
             int level = anv.getMenu().getCost();
-            if (level <= 0 || !anv.getMenu().getSlot(anv.getMenu().getResultSlot()).hasItem()) return;
+            if (level <= 0 || !anv.getMenu().getSlot(anv.getMenu().getResultSlot()).hasItem() || level == Integer.MAX_VALUE) return;
             List<Component> list = new ArrayList<>();
             list.add(Component.literal(I18n.get("info.apotheosis.anvil_at", level)).withStyle(ChatFormatting.UNDERLINE, ChatFormatting.GREEN));
             int expCost = EnchantmentUtils.getTotalExperienceForLevel(level);
