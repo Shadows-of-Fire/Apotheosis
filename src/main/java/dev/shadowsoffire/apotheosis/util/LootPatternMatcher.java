0602639bf63717ac9afb3eb67c0a44c13f117b90
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-09-03 18:31:48 -0700

    Make `LootPatternMatcher` a `LootItemCondition`

--- src/main/java/dev/shadowsoffire/apotheosis/util/LootPatternMatcher.java
+++ src/main/java/dev/shadowsoffire/apotheosis/util/LootPatternMatcher.java
@@ -6,13 +6,18 @@ import java.util.regex.Pattern;
 import org.spongepowered.include.com.google.common.base.Preconditions;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+import dev.shadowsoffire.apotheosis.Apoth;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
 
-public record LootPatternMatcher(Optional<String> domain, Pattern pathRegex) {
+public record LootPatternMatcher(Optional<String> domain, Pattern pathRegex) implements LootItemCondition {
 
-    public static final Codec<LootPatternMatcher> CODEC = RecordCodecBuilder.create(inst -> inst.group(
+    public static final MapCodec<LootPatternMatcher> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
         Codec.STRING.optionalFieldOf("domain").forGetter(LootPatternMatcher::domain),
         Codec.STRING.xmap(Pattern::compile, Pattern::toString).fieldOf("path_regex").forGetter(LootPatternMatcher::pathRegex))
         .apply(inst, LootPatternMatcher::new));
@@ -28,5 +33,19 @@ public record LootPatternMatcher(Optional<String> domain, Pattern pathRegex) {
 
     public boolean matches(ResourceLocation id) {
         return (this.domain.isEmpty() || this.domain.get().equals(id.getNamespace())) && this.pathRegex.matcher(id.getPath()).matches();
+    }
+
+    @Override
+    public boolean test(LootContext t) {
+        return this.matches(t.getQueriedLootTableId());
+    }
+
+    @Override
+    public LootItemConditionType getType() {
+        return Apoth.LootConditions.LOOT_TABLE_PATTERN_MATCHER;
+    }
+
+    public static LootItemCondition.Builder matchesTables(String domain, String regex) {
+        return () -> new LootPatternMatcher(domain.isBlank() ? Optional.empty() : Optional.of(domain), Pattern.compile(regex));
     }
 }
