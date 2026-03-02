d4efb2f036358598dbb81d67ebd810d9a0211112
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2026-03-02 16:54:06 +0800

    Add level check in doPostAttack method

--- src/main/java/dev/shadowsoffire/apotheosis/ench/replacements/BaneEnchant.java
+++ src/main/java/dev/shadowsoffire/apotheosis/ench/replacements/BaneEnchant.java
@@ -53,7 +53,7 @@ public class BaneEnchant extends DamageEnchantment {
      */
     @Override
     public void doPostAttack(LivingEntity user, Entity target, int level) {
-        if (target instanceof LivingEntity livingentity) {
+        if (target instanceof LivingEntity livingentity && level > 0) {
             if (this.attrib != MobType.UNDEFINED && livingentity.getMobType() == this.attrib) {
                 int i = 20 + user.getRandom().nextInt(10 * level);
                 livingentity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, i, 3));
